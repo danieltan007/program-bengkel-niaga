171f9a27b7ad4accfadf42b6b296ff1c48a8cfb3
danieltan007 <capricondaniel007@yahoo.co.id>
2022-02-23 21:13:22 +0700

    minor bug fix

--- eceran/do_kasirtambahtemp.php
+++ eceran/do_kasirtambahtemp.php
@@ -8,5 +8,6 @@ $kode = $_GET['kode'];
 $sql = mysqli_query($conn, "select * from `tabel barang pusat` where kd_brg = '$kode'");
 $data = mysqli_fetch_array($sql);
 
-$sql1 = "insert into `tabel barang temp` values ($data[kd_brg], $data[nm_brg], $data[hrg_brg], $data[mrk_brg], 0, $data[supp_brg], '$user')";
+$sql1 = "insert into `tabel barang temp` values ('$data[kd_brg]', '$data[nm_brg]', '$data[hrg_jual]', '$data[mrk_brg]', 0, '$data[supplier]', '$user')";
+echo "sql1 : $sql1";
 $input = mysqli_query($conn, $sql1);
