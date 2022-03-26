5aa49cd12de2a50cfe8392f04fb785a784232f33
danieltan007 <capricondaniel007@yahoo.co.id>
2022-03-26 22:45:44 +0700

    minor bug fix

--- grosir/do_kasirceknama.php
+++ grosir/do_kasirceknama.php
@@ -3,7 +3,7 @@
 
      $nama = $_POST['nama'];
 
-$sql = "select * from `tabel transaksi grosir` where nama = '$nama'";
+$sql = "select * from `riwayat pembelian` where nama = '$nama'";
      $data = mysqli_num_rows(mysqli_query($conn, $sql));
 if ($data > 0) {
      echo 1;
